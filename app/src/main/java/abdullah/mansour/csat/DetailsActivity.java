43454ee4f34312571b5008a65212cdb5e34c5992
abdullahmansss <abdullah.mansour97@gmail.com>
2019-03-05 01:31:24 +0200

    rate

--- app/src/main/java/abdullah/mansour/csat/DetailsActivity.java
+++ app/src/main/java/abdullah/mansour/csat/DetailsActivity.java
@@ -1,15 +1,70 @@
 package abdullah.mansour.csat;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity
 {
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        floatingActionButton = findViewById(R.id.add_review);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showReviewDialog();
+            }
+        });
+    }
+
+    private void showReviewDialog()
+    {
+        final Dialog dialog = new Dialog(DetailsActivity.this);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.rate_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes();
+        dialog.setCancelable(false);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        Button cancel = dialog.findViewById(R.id.company_rating_cancel_btn);
+        Button add_review = dialog.findViewById(R.id.company_rating_review_btn);
+
+        final EditText review_content = dialog.findViewById(R.id.company_review_field);
+        final RatingBar ratingBar = dialog.findViewById(R.id.company_ratingbar);
+
+        cancel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 }
